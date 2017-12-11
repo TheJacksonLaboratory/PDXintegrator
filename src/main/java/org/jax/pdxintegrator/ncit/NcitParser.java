37a8c07d373a636f6e052ecfba29fb65e0cbbfa1
pnrobinson <peter.robinson@charite.de>
2017-12-10 21:39:21 -0500

    Ncit parser prototype

---
+++ src/main/java/org/jax/pdxintegrator/ncit/NcitParser.java
@@ -0,0 +1,19 @@
+package org.jax.pdxintegrator.ncit;
+
+import com.github.phenomics.ontolib.io.obo.OboParser;
+
+public class NcitParser {
+
+    private final String ncitPath;
+
+    public NcitParser(String path) {
+        ncitPath=path;
+    }
+
+
+    public void parseNcit() {
+        OboParser parser = new OboParser(true);
+    }
+
+
+}
